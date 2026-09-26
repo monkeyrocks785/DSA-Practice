237e1d8513ac5b544f3b969da2ab4bc9b5d879ba
Mayank Garg <76584858+monkeyrocks785@users.noreply.github.com>
2026-09-26 11:32:01 +0530

    Time: 0 ms (100%), Space: 19.1 MB (87.62%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -1,6 +1,6 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n <= 2:
+        if (n <= 2):
             return n
 
         dp = [0] * (n + 1)
@@ -10,5 +10,5 @@ class Solution:
 
         for i in range(3, n + 1):
             dp[i] = dp[i - 1] + dp[i - 2]
-
+        
         return dp[n]
